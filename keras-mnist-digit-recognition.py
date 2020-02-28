59d1a295b62e4e6151f8c2ad90d86db704e85d4d
Ankit Anurag <ankit.anurag23@gmail.com>
2020-02-28 21:08:53 +0530

    added image; added identification in image

--- keras-mnist-digit-recognition.py
+++ keras-mnist-digit-recognition.py
@@ -41,3 +41,55 @@ classifier.fit(X_train, y_train,
               batch_size=200,
               epochs=3,
               validation_data=(X_test, y_test))
+
+classifier.summary()
+classifier.save('digit_recognizer.h5')
+
+#performing digit recognition on external handwritten digits
+import cv2
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+
+#read image file
+img = cv2.imread("./Number.jpeg")
+if img is None:
+    print("No such file exists")
+plt.imshow(img, cmap='gray')
+plt.show()
+
+#convert to grayscale
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(img.shape)
+plt.imshow(img, cmap='gray')
+plt.show()
+
+#threshholding - digits2black
+_, thresh = cv2.threshold(img, 90, 255, cv2.THRESH_BINARY_INV)
+plt.imshow(thresh, cmap='gray')
+plt.show()
+
+#find contours
+contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+img_copy = img.copy()
+digits = []
+for contour in contours:
+    x,y,w,h = cv2.boundingRect(contour)
+    cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    digit = img[y:y+h, x:x+w]
+    digit = np.pad(cv2.resize(digit, (18, 18)), ((5, 5), (5, 5)), 'constant', constant_values=255)
+    _, digit = cv2.threshold(digit, 100, 255, cv2.THRESH_BINARY_INV)
+    digits.append(digit)
+    
+#show image with bounding rectangle
+plt.imshow(img_copy, cmap='gray')
+plt.show()
+
+#Shaping the 
+#processing the digits
+X_pred = np.array(digits) / 255
+for X in X_pred:
+    print('----------\n----------')
+    prediction = classifier_loaded(X.reshape(1, img_rows, img_cols, 1))
+    plt.imshow(X.reshape(img_rows, img_cols), cmap='gray')
+    plt.show()
+    print(np.argmax(prediction, axis=1))
